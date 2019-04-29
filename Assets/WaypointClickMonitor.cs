16859258f9684343f049cd5e91ad968c45a337ef
Stuffdude99 <wadhwadhananjay@gmail.com>
2019-04-28 23:33:56 -0700

    fsm source files

---
+++ Assets/WaypointClickMonitor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointClickMonitor : MonoBehaviour {
+
+	public GameObject waypointObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			var playfield = GameObject.Find("Plane").GetComponent<Collider>();
+			RaycastHit hitPoint;
+			if (playfield.Raycast(ray,out hitPoint,float.MaxValue)) {
+				Vector3 newWaypoint = hitPoint.point;
+				Debug.Log ("Waypoint position: " + newWaypoint.ToString());
+				var playerWaypointList = GameObject.Find ("free_male_1").GetComponent<WaypointMovement>().waypoints;
+				Vector3 waypointObjectPoint = newWaypoint;
+				waypointObjectPoint.y = 0.5f;
+				Instantiate (waypointObject, waypointObjectPoint,new Quaternion());
+				playerWaypointList.Enqueue (newWaypoint);
+				Debug.Log("New waypoint list: ");
+				foreach (Vector3 i in playerWaypointList) {
+					Debug.Log (i.ToString ());
+				}
+			}
+		}
+	}
+}
---
+++ Assets/WaypointMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointMovement : MonoBehaviour {
+
+	public Queue<Vector3> waypoints;
+	private bool isWalking;
+	public float speed = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		waypoints = new Queue<Vector3> ();
+		isWalking = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		var anim = this.GetComponent<Animator> ();
+		if (waypoints.Count > 0) {
+			if (!isWalking) {
+				anim.Play ("walk");
+			}
+			if (Vector3.Distance (this.transform.position, waypoints.Peek ()) > 0.1f) {
+				this.transform.position = Vector3.MoveTowards (this.transform.position, waypoints.Peek (), speed * Time.deltaTime);
+			} else {
+				waypoints.Dequeue ();
+			}
+
+		} else {
+			anim.Play ("idle");
+		}
+	}
+}
